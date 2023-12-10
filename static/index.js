5743f01c7b7203e9c8701f4d4044123fcd33dd59
Samujjwal Kumar <kumarsamujjwal@gmail.com>
2023-12-09 23:08:11 -0500

    Final Commit

--- static/index.js
+++ static/index.js
@@ -1,11 +1,12 @@
 
 function submitRecipe() {
+    // Preserve your styling and visibility changes
     document.getElementById('section3').style.backgroundImage = '';
     document.getElementById('section3').style.background = '#487b7f';
     document.getElementById('tick1').classList.remove('hidden');
 
-    var field = document.getElementById('recipe')
-    var button = document.getElementById('submit-recipe')
+    var field = document.getElementById('recipe');
+    var button = document.getElementById('submit-recipe');
 
     if (field) {
         field.style.display = 'none';
@@ -13,16 +14,42 @@ function submitRecipe() {
     if (button) {
         button.style.display = 'none';
     }
+
+    // Submit the form using AJAX
+    $.ajax({
+        type: 'POST',
+        url: '/submit',
+        data: $('#recipe-form').serialize(),
+        success: function() {
+            // Redirect to loading page
+            window.location.href = '/loading';
+        }
+    });
 }
 
+
+
+
 function submitZip() {
+
+    var field = document.getElementById('zip')
+    var button = document.getElementById('zip-submit')
+    var zipCode = field.value;
+
+    if (!isValidUSZip(zipCode)) {
+        // Display an error message
+        alert("Invalid ZIP!! Please enter a valid US ZIP code.");
+
+        // Prevent the form submission
+        return false;
+    }
+
     document.getElementById('section2').style.backgroundImage = '';
     document.getElementById('section2').style.background = '#364851';
     document.getElementById('tick2').classList.remove('hidden');
     document.getElementById('section3').classList.add('visible');
 
-    var field = document.getElementById('zip')
-    var button = document.getElementById('zip-submit')
+    
 
     if (field) {
         field.style.display = 'none';
@@ -32,16 +59,20 @@ function submitZip() {
     }
 
 
+    // If the zip code is valid, allow the form submission
+    return true;
+
+
 }
 
-// function updateText() {
-//     var text = document.getElementById('ingredients');
-//     text.innerText = "Your new dynamic text here";
-// }
+function isValidUSZip(sZip) {
+    return /^\d{5}(-\d{4})?$/.test(sZip);
+}
+
 
 window.onload = function() {
     var typed = new Typed('#animated-paragraph', {
-        strings: ['How to use the application?', 'Step 1: Enter a zip code.', 'Step 2: Enter the name of the recipe.', 'Step 3: The crawler fetches the ingredients for the recipe. Next, it crawls the web to fetch you the best price for the ingredients from stores near you.'],
+        strings: ['How to use the application?', 'Step 1: Zip code.', 'Step 2: Your favorite recipe.', 'Step 3: Get ingredients and best prices!'],
         typeSpeed: 50,
         onComplete: function() {
             document.getElementById('section2').classList.add('visible');
